f6b2b86d0bbb932f3abd44cb29b6ab583dad5643
Melody Kao <melody.ch.kao@gmail.com>
2022-01-12 11:24:26 +0100

    create an error handling

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});

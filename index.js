57f6fa6ef9961efe404fa7aff8e378c6e21b80b0
Melody Kao <melody.ch.kao@gmail.com>
2022-01-14 14:35:27 +0100

    Create the Express code to route all of the endpoints you listed

--- index.js
+++ index.js
@@ -1,18 +1,106 @@
-const express = require('express');
+const express = require('express'),
+bodyParser = require('body-parser'),
+uuid = require('uuid'),
 morgan = require('morgan');
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(morgan('common'));
-
+//get a list of movies
 app.get('/movies', (req, res) => {
-let movies=[
-{name:"Iron Man"},
-{name:"Incredible Hulk"},
-{name:"Iron Man 2"},
-]
-res.json(movies);
+  let movies=[
+  {name:"Iron Man"},
+  {name:"Incredible Hulk"},
+  {name:"Iron Man 2"},
+  ]
+  res.json(movies);
+  });
+//get data about a single movie
+app.get('/movies/:name', (req, res) =>{
+  res.json(movies.find((movies)=>
+    {return movies.name === req.params.name
+    }));
+  });
+
+//get data about a genre
+app.get('/movies/:genre',(req, res) =>{
+  res.json(movies.find((movie)=>
+    {return movies.genre === req.params.genre
+    }));
+  });
+
+//get data about a director by name
+app.get('/movies/directors/:name',(req, res)=>{
+  res.json(directors.find((director) =>
+    {return director.name === req.params.name
+    }));
+  });
+
+
+//add new users to register
+app.post('/movies/users', (req, res)=> {
+  let newUser = req.body;
+
+  if (!newUser.name) {
+    const message = 'Missing name in request body';
+    res.status(400).send(message);
+  } else {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
 });
+//update username
+app.put('/users/:name', (req, res)=>{
+  let user = users.fine((user)=> {
+    return user.name === req.params.name
+  });
+
+  if (user){
+    user.userName [req.params.userName] = parseInt(req.params.userName);
+    res.status(201).send('User'+req.params.userName +'was updated');
+  } else {
+    res.status(404).send('User with the name'+ req.params.userName +'was not found.');
+  }
+});
+
+//add a movie to their list of favorites
+app.post('/users/listofffavorites',(req, res)=>{
+  let newMovie = req.body;
+  if(!newMovie.name) {
+    const message ='Successfully added';
+    res.status(400).send(message);
+  } else {
+    movies.push(newMovie);
+    res.status(201).send(newMovie);
+  }
+});
+
+//delete movie
+app.delete('/users/listoffavorites',(req, res)=>{
+  let movie = movies.find((movies) => {
+    return movie.name === req.params.name
+  });
+
+  if (movie){
+    movies = movies.filer((obj)=> {
+    res.status(201).send('Movie '+ req.params.name + ' was deleted');
+  });
+
+//remove a user
+app.delete('/users',(req, res)=>{
+  let user = users.find((user)=>
+  {return user.id === req.params.id
+   });
+
+  if (user) {
+      users = users.filter((obj)=>{
+        return obj.id !==req.params.id
+      });
+      res.status(201).send('Student' + req.params.id + ' was deleted.')
+    }
+  });
 
 app.get('/', (req, res) => {
   res.send('Welcome to my movie app!');
@@ -26,5 +114,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+  console.log('Your app is listening on port 8080');
 });

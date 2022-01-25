7540b578c4a7719c979506197726b2b01bc83311
Melody Kao <melody.ch.kao@gmail.com>
2022-01-25 17:39:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -185,6 +185,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',()=>{
-  console.log('Listening on Port '+ port);
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
 });

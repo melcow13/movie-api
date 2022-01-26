af7c1d36c2df0a417950c0adc1d229c37f2ab9d1
Melody Kao <melody.ch.kao@gmail.com>
2022-01-26 10:30:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.post('/users',[
   check('Username','Username is required').isLength({min:5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password','Password is required').not().isEmpty(),
-  check('Email','Email does not appear to be valid').isEmpty()], (req, res) => {
+  ], (req, res) => {
     let errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(422).json({errors: errors.array()});

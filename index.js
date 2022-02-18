a44ea1bc71b9a2737012d228d1e2943c98b65e13
Melody Kao <melody.ch.kao@gmail.com>
2022-02-18 17:29:45 +0800

    get data about user

--- index.js
+++ index.js
@@ -50,6 +50,18 @@ app.get('/movies/:title',passport.authenticate('jwt',{session: false}), (req, re
     });
 });
 
+//get data about a user
+app.get('/users/:username',passport.authenticate('jwt',{session: false}), (req, res) => {
+  Users.findOne({Username: req.params.username})
+    .then((user)=>{
+      res.json(user);
+    })
+    .catch((err)=>{
+      console.error(err);
+      res.status(500).send('Error: '+ err);
+    });
+});
+
 //get data about a genre
 app.get('/movies/genres/:genre',passport.authenticate('jwt',{session: false}),(req, res) => {
   Movies.findOne({'Genre.Name': req.params.genre })
